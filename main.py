3dc9a95a283477c919fac25dd3564806c0432cd2
zapper-95 <jbtl68@durham.ac.uk>
2023-04-16 13:15:27 +0100

    Messy code with some ideas on all different processing techniques

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import argparse
 import os
 import cv2
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 def edge_sharpener(img,kernel_size,kernel_size2, k):    
     gaussian_img = cv2.GaussianBlur(img,(kernel_size, kernel_size), cv2.BORDER_DEFAULT)
@@ -51,31 +51,72 @@ def main():
     # Process each image
     for file in files:
         # Read image
+        print("Processing image: ", file)
         try:
             # if the file ends in .png, .jpg, or .jpeg, then process it
             if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
                 img = cv2.imread(args.path + "/" + file)
 
 
+                img = fix_perspective(img)
+                '''
+                Making an ellipse that fits over the image so I can apply a transformation to make it a circle
+                                overlay = img.copy()
 
-                sigma_r = 250
-                sigma_s = 600
+                alpha = 0.5
 
-                # apply billateral filtering
-                #img = cv2.bilateralFilter(img, 15, sigma_r, sigma_s, borderType=cv2.BORDER_REPLICATE)
+                # detect the ellipse and transform the distorted perspective so that it is a circle
+                overlay = cv2.ellipse(overlay, (130,125), (105,145), -7, 0, 360, (255,255,255), -1)
+                img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
+                
+                                ellipse = cv2.ellipse(img, (130,125), (105,145), -7, 0, 360, (255,255,255), -1)
+                circle = cv2.circle(img, (130,125), 120, (255,255,255), -1)
+                '''
+                # Warping: the images are distorted. This issue can potentially be resolved using projective transformations, so that objects within the images look as they should.
+
+                
+                #ellipse = cv2.ellipse(img, (130,125), (105,145), -7, 0, 360, (255,255,255), -1)
+                
+                
 
 
-                # apply non local means denoising
-                #img = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21) 
 
-                # remove salt and pepper noise
-                img = cv2.medianBlur(img,3)
+                # Write a function to deal with the following problem:
+                # Colour channel imbalance: the information contained within the colour channels of the images is not balanced - i.e. some channels might be darker/brighter than others.
+                # This can be resolved by applying a histogram equalisation to the image.
+
+                
+
+                plt.hist(img.ravel(),256,[0,256])
+               
+
+                #img[:,:,0] = cv2.equalizeHist(img[:,:,0])
+                #plt.hist(img.ravel(),256,[0,256])
+
+
+
+                #plt.show()
+                cv2.imshow("image", img)
+                cv2.waitKey(0)
+
+                # use contrast stretching to improve the contrast of the image
+                img = alter_contrast_brightness(img)
+                
+                #draw the image
+                #cv2.imshow("image", img)
+                #cv2.waitKey(0)
+                #break
+                
+                
+
+
+                img = remove_noise(img)
                 
                 # sharpen the edges
                 #img = edge_sharpener(img,5,3,0.8)
 
 
-                cv2.imwrite("results/" + file, img)
+                cv2.imwrite("Results/" + file, img)
 
         
         # print exception and continue
@@ -84,6 +125,47 @@ def main():
             continue
 
 
+def fix_perspective(img):
+    # ellipse = cv2.ellipse(img, (130,125), (105,145), -7, 0, 360, (255,255,255), -1)
+    #circle = cv2.circle(img, (130,125), 120, (255,255,255), -1)
+    
+    rows, cols = img.shape[:2]
+
+    src_points = np.float32([[25,125], [235,125], [130,0], [130,250]])
+    
+    #draw the src points on the image. Make the colour green
+
+    for point in src_points:
+        # get the x and y coordinates of the point
+        x = int(point[0])
+        y = int(point[1])
+        #img = cv2.circle(img, (x,y), radius=3, color=(0, 255, 0), thickness=-1,)
+    
+    dst_points = np.float32([[0,125], [250,125], [130,0], [130,250]]) 
+    projective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+    return cv2.warpPerspective(img, projective_matrix, (cols,rows))
+
+
+
+def remove_noise(img):
+    sigma_r = 200
+    sigma_s = 200
+        # apply billateral filtering
+    #img = cv2.bilateralFilter(img, 3, sigma_r, sigma_s, borderType=cv2.BORDER_REPLICATE)
+
+
+    # apply non local means denoising
+    #img = cv2.fastNlMeansDenoisingColored(img, 8, 8, 21, 7) 
+    
+    
+    #remove salt and pepper noise
+    img = cv2.medianBlur(img,3)
+    return img
+
+def alter_contrast_brightness(img):
+    #correct contrast and brightness
+    img = cv2.convertScaleAbs(img, alpha=0.8, beta=-20)
+    return img
 
 if __name__ == "__main__":
     main()
