7975660ddd10684a4a908333de22655589defdfd
zapper-95 <jbtl68@durham.ac.uk>
2023-04-17 13:25:07 +0100

    Added fourier transform and butterworth low pass filter

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import os
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import math
 
 def edge_sharpener(img,kernel_size,kernel_size2, k):    
     gaussian_img = cv2.GaussianBlur(img,(kernel_size, kernel_size), cv2.BORDER_DEFAULT)
@@ -61,18 +62,20 @@ def main():
             # if the file ends in .png, .jpg, or .jpeg, then process it
             if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
                 img = cv2.imread(args.path + "/" + file)
-
+                
+                dft(img)
                 #show_image(img)
 
+                #show_image(img, "Original Image")
 
                 img = add_in_painting(img)
                 img = fix_perspective(img)
-                img = CLACHE(img)
 
                 img = remove_noise(img)
+                img = CLACHE(img)
 
                 #show_image(img)
-
+                #cv2.waitKey(0)
                 cv2.imwrite("Results/" + file, img)
 
         
@@ -82,9 +85,9 @@ def main():
             continue
 
 
-def show_image(img):
-    cv2.imshow("image", img)
-    cv2.waitKey(0)
+def show_image(img, name="image"):
+    cv2.imshow(name, img)
+
 
 
 def plot_histogram(img):
@@ -126,18 +129,18 @@ def colour_correction(img):
 
 
 def remove_noise(img):
-    sigma_r = 200
-    sigma_s = 400
+    sigma_r = 10
+    sigma_s = 30
         # apply billateral filtering
-    #img = cv2.bilateralFilter(img, 3, sigma_r, sigma_s, borderType=cv2.BORDER_REPLICATE)
+    #img = cv2.bilateralFilter(img, 9, sigma_r, sigma_s, borderType=cv2.BORDER_REPLICATE)
 
 
     # apply non local means denoising
-    #img = cv2.fastNlMeansDenoisingColored(img,None,5,0,7,21)
+    #img = cv2.fastNlMeansDenoisingColored(img,None,3,3,7,21)
     
     
     #remove salt and pepper noise
-    img = cv2.medianBlur(img,3)
+    #img = cv2.medianBlur(img,3)
     #img  = cv2.bilateralFilter(img,9,75,75)
     return img
 
@@ -160,7 +163,7 @@ def alter_contrast_brightness(img):
 
 def CLACHE(img):
     ycrcb_img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
     ycrcb_img[:, :, 0] = clahe.apply(ycrcb_img[:, :, 0])
     img = cv2.cvtColor(ycrcb_img, cv2.COLOR_YCrCb2BGR)
 
@@ -171,7 +174,124 @@ def alter_contrast_brightness2(img):
 
     return img
 
+def dft(img, radius=50, order=5):
+    
+    # convert to ycrb
+    ycrcb_img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
 
+    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    width = int(gray_frame.shape[1])
+    height = int(gray_frame.shape[0])
+    dim = (width, height)
+
+    # set up optimized DFT settings
+
+    nheight = cv2.getOptimalDFTSize(height)
+    nwidth = cv2.getOptimalDFTSize(width)
+
+    # Performance of DFT calculation, via the FFT, is better for array
+    # sizes of power of two. Arrays whose size is a product of
+    # 2's, 3's, and 5's are also processed quite efficiently.
+    # Hence we modify the size of the array to the optimal size (by padding
+    # zeros) before finding DFT.
+
+    pad_right = nwidth - width
+    pad_bottom = nheight - height
+    nframe = cv2.copyMakeBorder(
+        gray_frame,
+        0,
+        pad_bottom,
+        0,
+        pad_right,
+        cv2.BORDER_CONSTANT,
+        value=0)
+
+        # perform the DFT and get complex output
+
+    dft = cv2.dft(np.float32(nframe), flags=cv2.DFT_COMPLEX_OUTPUT)
+
+    # shift it so that we the zero-frequency, F(0,0), DC component to the
+    # center of the spectrum.
+
+    dft_shifted = np.fft.fftshift(dft)
+
+
+
+    lp_filter = create_butterworth_low_pass_filter(nwidth, nheight, radius, order)
+    lo_dft_filtered = cv2.mulSpectrums(dft_shifted, lp_filter, flags=0)
+
+    # shift back to original quaderant ordering
+
+    lo_dft = np.fft.fftshift(lo_dft_filtered)
+
+    # recover the original image via the inverse DFT
+
+    lo_filtered_img = cv2.dft(lo_dft, flags=cv2.DFT_INVERSE)
+
+    # normalized the filtered image into 0 -> 255 (8-bit grayscale) 
+    # so we can see the output
+
+    # low pass filter output
+
+    lo_min_val, lo_max_val, lo_min_loc, lo_max_loc = \
+        cv2.minMaxLoc(lo_filtered_img[:, :, 0])
+    lo_filtered_img_normalised = lo_filtered_img[:, :, 0] * (
+        1.0 / (lo_max_val - lo_min_val)) + ((-lo_min_val) / (lo_max_val - lo_min_val))
+    lo_filtered_img_normalised = np.uint8(lo_filtered_img_normalised * 255)
+
+    # calculate the magnitude spectrum and log transform + scale for visualization
+
+
+    lo_magnitude_spectrum = np.log(cv2.magnitude(
+        lo_dft_filtered[:, :, 0], lo_dft_filtered[:, :, 1]))
+
+    magnitude_spectrum = np.log(cv2.magnitude(
+        dft_shifted[:, :, 0], dft_shifted[:, :, 1]))
+
+    # create 8-bit images to put the magnitude spectrum into
+
+    magnitude_spectrum_normalised = np.zeros((nheight, nwidth, 1), np.uint8)
+
+    # normalized the magnitude spectrum into 0 -> 255 (8-bit grayscale) so
+    # we can see the output
+
+    cv2.normalize(
+        np.uint8(magnitude_spectrum),
+        magnitude_spectrum_normalised,
+        alpha=0,
+        beta=255,
+        norm_type=cv2.NORM_MINMAX)
+
+    # convert back to colour for visualisation
+
+    gray_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2BGR)
+    magnitude_spectrum_normalised = cv2.cvtColor(magnitude_spectrum_normalised, cv2.COLOR_GRAY2BGR)
+    lo_filtered_img_normalised = cv2.cvtColor(lo_filtered_img_normalised, cv2.COLOR_GRAY2BGR)
+    lp_filter_vis = cv2.cvtColor(np.uint8(lp_filter[:, :, 0] * 255), cv2.COLOR_GRAY2BGR)
+
+    cv2.imshow('original', gray_frame)
+    cv2.imshow('magnitude spectrum', magnitude_spectrum_normalised)
+    cv2.imshow('low pass filtered image', lo_filtered_img_normalised)
+    cv2.imshow('low pass filter', lp_filter_vis)
+
+    cv2.waitKey(0)
+    
+
+def create_low_pass_filter(width, height, radius):
+    lp_filter = np.zeros((height, width, 2), np.float32)
+    cv2.circle(lp_filter, (int(width / 2), int(height / 2)),
+               radius, (1, 1, 1), thickness=-1)
+    return lp_filter
+
+def create_butterworth_low_pass_filter(width, height, d, n):
+    lp_filter = np.zeros((height, width, 2), np.float32)
+    centre = (width / 2, height / 2)
+
+    for i in range(0, lp_filter.shape[1]):  # image width
+        for j in range(0, lp_filter.shape[0]):  # image height
+            radius = max(1, math.sqrt(math.pow((i - centre[0]), 2.0) + math.pow((j - centre[1]), 2.0)))
+            lp_filter[j, i] = 1 / (1 + math.pow((radius / d), (2 * n)))
+    return lp_filter
 
 if __name__ == "__main__":
     main()
